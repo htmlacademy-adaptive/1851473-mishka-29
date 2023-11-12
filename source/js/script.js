54440ba310f3fee46a7db5e3d1b390891222e26b
Igor <3-tv-n@mail.ru>
2023-11-12 21:20:36 +0300

    делаю шапку для tablet

---
+++ source/js/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('.header__burger');
+const navigation = document.querySelector('.navigation');
+
+navigation.classList.remove('navigation--no-js');
+navigation.classList.add('navigation--close');
+btn.classList.remove('header__burger--no-js');
+
+btn.addEventListener('click', function() {
+  if (btn.classList.contains('header__burger--open')) {
+    btn.classList.remove('header__burger--open');
+    navigation.classList.remove('navigation--open');
+    navigation.classList.add('navigation--close');
+  } else {
+    btn.classList.add('header__burger--open');
+    navigation.classList.remove('navigation--close');
+    navigation.classList.add('navigation--open');
+  }
+})
+
+
